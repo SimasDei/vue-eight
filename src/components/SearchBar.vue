4e6c0c670b8a633fc79c9019f88dd374e9ef58a4
Simas <simonas@digitouch.lt>
2019-05-09 13:30:41 +0300

    setup vue from scratch

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input type="text" @input="onInput">
+  </div>
+</template>
+<script>
+export default {
+  name: "SearchBar",
+  methods: {
+    onInput: function(event) {
+      this.$emit("termChange", event);
+    }
+  }
+};
+</script>
